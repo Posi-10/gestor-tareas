0bad3b59481887826ef03a7dd9226f3ab294973a
Miguel Angel Posada Ibañez <64515605+Posi-10@users.noreply.github.com>
2021-06-23 06:57:06 -0500

    Actualizacion

--- app/dependencias.php
+++ app/dependencias.php
@@ -1,8 +1,6 @@
 <link rel="stylesheet" href="<?=SERVIDOR?>lib/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="<?=SERVIDOR?>lib/fontawesome/css/all.min.css">
+<link rel="stylesheet" href="<?=SERVIDOR?>lib/fontawesome-free-5.15.3-web/css/all.min.css">
 <link rel="stylesheet" href="<?=SERVIDOR?>raw/css/styles.css">
-
-<script src="<?=SERVIDOR?>lib/jquery/jquery-3.6.0.min.js"></script>
-<script src="<?=SERVIDOR?>lib/popper/popper.min.js"></script>
+<script src="<?=SERVIDOR?>lib/jquery/jquery3.6.0.js"></script>
+<script src="<?=SERVIDOR?>lib/popper/popper.js"></script>
 <script src="<?=SERVIDOR?>lib/bootstrap/js/bootstrap.min.js"></script>
-<script src="<?=SERVIDOR?>lib/fontawesome/js/all.min.js"></script>
--- view/registro.php
+++ view/registro.php
@@ -1,91 +1,92 @@
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-9">
-      <form class="shadow p-4 bg-form rounded-3">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12 text-center">
-              <h5>Registro</h5>
-            </div>
-          </div>
-          <hr class="hr-blanco">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="nombres">Nombre(s)</label>
-                      <input type="text" class="form-control form-control-sm rounded-pill" id="nombres" name="nombres" placeholder="Ingresa Nombres">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form class="shadow p-4 rounded-bottom fondo_formularios">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h5>Registro</h5>
+                        </div>
                     </div>
-                  </div>
+                    <hr class="divisor_horizontal">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="nombres">Nombre(s)</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="nombres" name="nombres" placeholder="Ingresar nombres">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="apellido_paterno">Apellido paterno</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="apellido_paterno" name="apellido_paterno" placeholder="Ingresar apellido paterno">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="apellido_materno">Apellido materno</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="apellido_materno" name="apellido_materno" placeholder="Ingresar apellido materno">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="sexo">Sexo:</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="sexo" name="sexo" placeholder="Ingresar sexo">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="fecha_nacimiento">Nacimiento:</label>
+                                            <input type="date" class="form-control form-control-sm rounded-pill" id="fecha_nacimiento" name="fecha_nacimiento">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="email"><i class="fas fa-at mr-2"></i>Email:</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="email" name="email" placeholder="Ingresar email">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="contrasenia"><i class="fas fa-lock mr-2"></i>Contraseña:</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="contrasenia" name="contrasenia" placeholder="Ingresar contraseña">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="confirmar_contrasenia"><i class="fas fa-lock mr-2"></i>Contraseña:</label>
+                                            <input type="text" class="form-control form-control-sm rounded-pill" id="confirmar_contrasenia" name="confirmar_contrasenia" placeholder="Ingresar contraseña">
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-12 text-center">
+                            <span class="btn btn-sm rounded-pill botones" id="boton_registrar"><i class="fas fa-sign-in-alt mr-2"></i>Registrar</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="apellido_paterno">Apellido Paterno</label>
-                      <input type="text" class="form-control form-control-sm rounded-pill" id="apellido_paterno" name="apellido_paterno" placeholder="Ingresa tu primer apellido">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="appellido_materno">Apellido Materno</label>
-                      <input type="text" class="form-control form-control-sm rounded-pill" id="appellido_materno" name="appellido_materno" placeholder="ingresa tu segundo apellido">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="sexo">Sexo</label>
-                      <input type="text" class="form-control form-control-sm rounded-pill" id="sexo" name="sexo" placeholder="Ingresa tu sexo">
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="nacimiento">Nacimiento</label>
-                      <input type="date" class="form-control form-control-sm rounded-pill" id="nacimiento" name="nacimiento" placeholder="">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label for="email"><i class="fas fa-at mr-2"></i>E-mail</label>
-                      <input type="email" class="form-control form-control-sm rounded-pill" id="email" name="email" placeholder="Ingresa E-mail">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="pass"><i class="fas fa-unlock-alt mr-2"></i>Contraseña</label>
-                      <input type="password" class="form-control form-control-sm rounded-pill" id="pass" name="pass">
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                    <div class="form-group">
-                      <label for="confirmar_pass">Contraseña</label>
-                      <input type="password" class="form-control form-control-sm rounded-pill" id="confirmar_pass" name="confirmar_pass">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 text-center">
-              <span class="btn botones btn-sm rounded-pill"><i class="fas fa-sign-in-alt mr-2"></i>Registrar</span>
-            </div>
-          </div>
+            </form>
         </div>
-      </form>
     </div>
-  </div>
 </div>
